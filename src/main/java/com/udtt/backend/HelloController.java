62c6b3973281f1b1441524a6c01f3b8a1669f7b2
chae-ring <149919388+chae-ring@users.noreply.github.com>
2026-05-05 01:18:22 +0900

    init spring boot backend

---
+++ src/main/java/com/udtt/backend/HelloController.java
@@ -0,0 +1,18 @@
+package com.udtt.backend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello World";
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
+}
---
+++ src/test/java/com/udtt/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udtt.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
