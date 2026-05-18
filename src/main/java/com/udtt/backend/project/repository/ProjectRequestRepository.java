fedeb393142b10f74481e530e8d0775b7ed41f1b
chae-ring <149919388+chae-ring@users.noreply.github.com>
2026-05-19 01:39:01 +0900

    feat: add projectRequest repository

--- src/main/java/com/udtt/backend/project/repository/ProjectRequestRepository.java
+++ src/main/java/com/udtt/backend/project/repository/ProjectRequestRepository.java
@@ -1,4 +1,11 @@
 package com.udtt.backend.project.repository;
 
-public class ProjectRequestRepository {
+import com.udtt.backend.project.entity.ProjectRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProjectRequestRepository extends JpaRepository<ProjectRequest, Long> {
+
+    Optional<ProjectRequest> findByIdAndDeletedAtIsNull(Long id);
 }
