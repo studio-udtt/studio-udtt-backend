b164e5886d02e891ee7b2fcef8a676554e3516c5
chae-ring <149919388+chae-ring@users.noreply.github.com>
2026-05-19 19:15:09 +0900

    chore: add bearer auth to swagger0

--- src/main/java/com/udtt/backend/global/config/SwaggerConfig.java
+++ src/main/java/com/udtt/backend/global/config/SwaggerConfig.java
@@ -4,6 +4,9 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,6 +17,8 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
+        String securitySchemeName = "bearerAuth";
+
         return new OpenAPI()
                 .info(apiInfo())
                 .servers(List.of(
@@ -22,8 +27,17 @@ public class SwaggerConfig {
                                 .description("로컬 서버"),
                         new Server()
                                 .url("https://api.studio.udtt.org")
-                                .description("운영 서버")));
-
+                                .description("운영 서버")))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                );
     }
 
     private Info apiInfo() {
